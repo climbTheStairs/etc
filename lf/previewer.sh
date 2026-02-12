2a4e14871822f6931b6f440f1c8dc2eab517c7c8
climbTheStairs <yxtqwf+github@gmail.com>
2026-02-12 15:41:45 -0800

    init

---
+++ lf/previewer.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# dep: bat, chafa, tar, unzip
+readonly BS=$(printf '\b')
+
+f=$1
+w=$(($2 - 5))
+h=$3
+case "$f" in
+	*.tsv)							tsv.py < "$f" ;;
+	*.jpg|*.png|*.svg|*.webp)		chafa --size "${w}x${h}" "$f" ;;
+	*.mp[34]|*.m4[av]|*.mkv|*.webm)	mediainfo "$f" ;;
+	*.pdf)							pdftotext "$f" - ;;
+	*.docx)							pandoc "$f" --to markdown --columns "$w" ;;
+	*.tar)							tar -tvf "$f" ;;
+# TODO: mor gen xz, o0 compressn
+	*.tar.gz|*.tgz)					tar -tvzf "$f" ;;
+	*.tar.xz|*.txz)					tar -tvJf "$f" ;;
+	*.zip)							unzip -l "$f" ;;
+	#*.dv)							dvcode < "$f" ;;
+	*.json)							jq --color-output < "$f" ;;
+	*.[123456789]|*.[123456789p])	man -O "width=$w" "$f" | sed "s/.$BS"'\(.\)/\1/g' ;;
+	*.c|*.cpp|*.css|*.go|*.h|*.java|*.js|*.lua|*.html|*.py|*.sh|[Mm]akefile) # TODO: `*Makefile`?
+									highlight -O ansi "$f" ;;
+	*)
+		if [ "$(head -n 1 "$f" | cut -c 1-2)" = '#!' ]; then
+			highlight -O ansi "$f"
+		else
+			bat --force-colorization --wrap never --terminal-width "$w" "$f"
+			# https://github.com/gokcehan/lf/issues/534#issuecomment-2039527648
+			# fold -sw $(($2 - 2)) "$1"
+		fi
+		;;
+esac
+
---
+++ vim/arch.vim
@@ -0,0 +1,7 @@
+" macros
+let @r='8ldf.fr2df/4wcf/ismmdlk[f/xf#dE' " fmt @red comment url
+
+" default actions for diff files
+au BufWritePost /etc/nginx/*.conf !systemctl restart nginx
+
+ru dvorak.vim
